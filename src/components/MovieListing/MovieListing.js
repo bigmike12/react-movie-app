d5348dbf962478e8de4312b165c77610abcdc44b
bigmike12 <dj.buckwild12@gmail.com>
2021-12-28 15:04:00 +0100

    worked on the MovieCard.js and mapping the data

--- src/components/MovieListing/MovieListing.js
+++ src/components/MovieListing/MovieListing.js
@@ -1,30 +1,29 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import MovieCard from "../MovieCard/MovieCard";
+import "./MovieListing.scss";
 
 function MovieListing() {
   const { movies } = useSelector((state) => state.movies);
-  let renderMovies = movies.response;
+  let renderMovies;
 
-  {
-    renderMovies ? (
+  renderMovies =
+    movies.Response === "True" ? (
       movies.Search.map((movie, index) => {
-        <MovieCard key={index} data={movie} />;
+        console.log(movie.Title);
+        return <MovieCard key={index} data={movie} />;
       })
     ) : (
       <div className="movies-error">
         <h3>{movies.Error}</h3>
       </div>
     );
-  }
 
   return (
     <div className="movie-wrapper">
       <div className="movie-list">
         <h2>Movies</h2>
-        <div className="movie-container">
-          {renderMovies}
-        </div>
+        <div className="movie-container">{renderMovies}</div>
       </div>
     </div>
   );
