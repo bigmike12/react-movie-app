aa951695920c91192e5a3ea05bb4bdcefaf0c379
bigmike12 <dj.buckwild12@gmail.com>
2021-12-28 03:37:02 +0100

    added redux splice and worked on MovieListing.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,9 +2,13 @@ import React, { useEffect } from "react";
 import MovieListing from "../MovieListing/MovieListing";
 import movieApi from "../../common/apis/MovieApi";
 import { APIKEY } from "../../common/apis/MovieApiKey";
+import { useDispatch } from "react-redux";
+import { addMovies } from "../../features/movies/movieSlice";
 
 function Home() {
   const movieTest = "Harry";
+
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const fetchMovies = async () => {
@@ -13,11 +17,11 @@ function Home() {
         .catch((err) => {
           console.log(err);
         });
-      console.log(response.data)
+      dispatch(addMovies(response.data))
     };
 
-    fetchMovies()
-  },[]);
+    fetchMovies();
+  }, []);
   return (
     <div className="banner-img">
       <MovieListing />
